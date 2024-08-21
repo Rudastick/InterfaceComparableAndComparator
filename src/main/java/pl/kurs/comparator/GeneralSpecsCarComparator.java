77e27827e8b2c78b5c52f6e19c77aacca7aae3b4
Tomasz A. Piekarski <tomasz.a.piekarski@lppsa.com>
2024-08-21 18:58:46 +0200

    xxx: skasowanie test

--- src/main/java/pl/kurs/comparator/GeneralSpecsCarComparator.java
+++ src/main/java/pl/kurs/comparator/GeneralSpecsCarComparator.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 public class GeneralSpecsCarComparator implements Comparator<Car> {
     @Override
     public int compare(Car o1, Car o2) {
-        int result = o1.getProducer().compareTo(o2.getProducer());
+        int result = o1.getFproducer().compareTo(o2.getFproducer());
 
         if (result == 0) {
             result = o1.getModel().compareTo(o2.getModel());
