e83f902c1b3f2a12825acbc33d1e5cd3181e51ad
Tomasz A. Piekarski <tomasz.a.piekarski@lppsa.com>
2024-08-21 19:02:41 +0200

    xxx: skasowanie test

--- src/main/java/pl/kurs/comparator/GeneralSpecsCarComparator.java
+++ src/main/java/pl/kurs/comparator/GeneralSpecsCarComparator.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 public class GeneralSpecsCarComparator implements Comparator<Car> {
     @Override
     public int compare(Car o1, Car o2) {
-        int result = o1.getCproducer().compareTo(o2.getCproducer());
+        int result = o1.getXxxproducer().compareTo(o2.getXxxproducer());
 
         if (result == 0) {
             result = o1.getModel().compareTo(o2.getModel());
--- src/main/java/pl/kurs/model/Car.java
+++ src/main/java/pl/kurs/model/Car.java
@@ -3,15 +3,15 @@ package pl.kurs.model;
 import java.util.Objects;
 
 public class Car {
-    private String cproducer;
+    private String xxxproducer;
     private String model;
     private char modelCode;
     private int powerHp;
     private int torqueNm;
     private int vMaxInKph;
 
-    public Car(String cproducer, String model, char modelCode, int powerHp, int torqueNm, int vMaxInKph) {
-        this.cproducer = cproducer;
+    public Car(String xxxproducer, String model, char modelCode, int powerHp, int torqueNm, int vMaxInKph) {
+        this.xxxproducer = xxxproducer;
         this.model = model;
         this.modelCode = modelCode;
         this.powerHp = powerHp;
@@ -19,12 +19,12 @@ public class Car {
         this.vMaxInKph = vMaxInKph;
     }
 
-    public String getCproducer() {
-        return cproducer;
+    public String getXxxproducer() {
+        return xxxproducer;
     }
 
-    public void setCproducer(String cproducer) {
-        this.cproducer = cproducer;
+    public void setXxxproducer(String xxxproducer) {
+        this.xxxproducer = xxxproducer;
     }
 
     public String getModel() {
@@ -70,7 +70,7 @@ public class Car {
     @Override
     public String toString() {
         return "Car{" +
-                "producer='" + cproducer + '\'' +
+                "producer='" + xxxproducer + '\'' +
                 ", model='" + model + '\'' +
                 ", modelCode=" + modelCode +
                 ", powerHp=" + powerHp +
@@ -84,11 +84,11 @@ public class Car {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Car car = (Car) o;
-        return modelCode == car.modelCode && powerHp == car.powerHp && torqueNm == car.torqueNm && vMaxInKph == car.vMaxInKph && Objects.equals(cproducer, car.cproducer) && Objects.equals(model, car.model);
+        return modelCode == car.modelCode && powerHp == car.powerHp && torqueNm == car.torqueNm && vMaxInKph == car.vMaxInKph && Objects.equals(xxxproducer, car.xxxproducer) && Objects.equals(model, car.model);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(cproducer, model, modelCode, powerHp, torqueNm, vMaxInKph);
+        return Objects.hash(xxxproducer, model, modelCode, powerHp, torqueNm, vMaxInKph);
     }
 }
